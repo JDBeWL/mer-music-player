64551fa17611b5bc3e639234e9a2ef986a561e8a
JDBeWl <375889147@qq.com>
2025-07-10 10:32:15 +0800

    添加新功能，优化页面在不同分辨率以及在手机端的展示效果

--- frontend/src/components/PlayerControls.vue
+++ frontend/src/components/PlayerControls.vue
@@ -11,25 +11,41 @@
     <button @click="nextSong" aria-label="下一曲">
       <font-awesome-icon :icon="['fas', 'forward-step']" />
     </button>
+    
+    <button @click="showAddMusic = true" class="add-music-btn" aria-label="添加音乐">
+      <font-awesome-icon :icon="['fas', 'plus']" />
+    </button>
+    
+    <div class="modal-overlay" v-if="showAddMusic" @click.self="showAddMusic = false">
+      <PlaylistAdd @close="showAddMusic = false" @music-added="handleMusicAdded" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { usePlayerStore } from '@/stores/player'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlay, faPause, faForwardStep, faBackwardStep } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faPause, faForwardStep, faBackwardStep, faPlus } from '@fortawesome/free-solid-svg-icons'
+import PlaylistAdd from './PlaylistAdd.vue'
 import "@/assets/css/player-controls.css"
 
-library.add(faPlay, faPause, faForwardStep, faBackwardStep)
+library.add(faPlay, faPause, faForwardStep, faBackwardStep, faPlus)
 
 const store = usePlayerStore()
+const showAddMusic = ref(false)
 
 const playPause = () => store.playPause()
 const nextSong = () => store.nextSong()
 const prevSong = () => store.prevSong()
 const isPlaying = computed(() => store.isPlaying)
+
+const handleMusicAdded = (newMusic) => {
+  console.log('新音乐已添加:', newMusic)
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
